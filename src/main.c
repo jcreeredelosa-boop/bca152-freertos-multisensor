9cfb3a23f72850bb30799fc4a834d7d50d8d217d
jcreeredelosa-boop <jcree.redelosa@g.msuiit.edu.ph>
2026-09-15 20:02:13 +0800

    Add ESP32 startup message

--- src/main.c
+++ src/main.c
@@ -1,1 +1,14 @@
-void app_main() {}
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    printf("BCA152 FreeRTOS Multisensor\n");
+    printf("System starting...\n");
+
+    while (true)
+    {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
